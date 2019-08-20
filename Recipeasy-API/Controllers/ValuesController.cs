f5f884d3199bb98e61b0404201975e0f37c72b29
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2019-08-20 16:02:05 +0100

    changed API return value to test CD

--- Recipeasy-API/Controllers/ValuesController.cs
+++ Recipeasy-API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Recipeasy_API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Andrew", "Colin" };
         }
 
         // GET api/values/5
