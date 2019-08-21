378c09a0cb8e72093916f752f195b64fd841ce93
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2019-08-21 22:05:24 +0100

    reverting out some of previous change

--- Recipeasy-API/Program.cs
+++ Recipeasy-API/Program.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace Recipeasy_API
 {
@@ -16,21 +12,6 @@ namespace Recipeasy_API
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .ConfigureAppConfiguration((ctx, builder) =>
-            {
-                var keyVaultEndpoint = GetKeyVaultEndpoint();
-                if (!string.IsNullOrEmpty(keyVaultEndpoint))
-                {
-                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                    var keyVaultClient = new KeyVaultClient(
-                        new KeyVaultClient.AuthenticationCallback(
-                            azureServiceTokenProvider.KeyVaultTokenCallback));
-                    builder.AddAzureKeyVault(
-                        keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
-                }
-            }
-            ).UseStartup<Startup>();
-
-        private static string GetKeyVaultEndpoint() => "https://recipeasy-keys.vault.azure.net/";
+                .UseStartup<Startup>();
     }
 }
