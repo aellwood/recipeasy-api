4972814ae80f04a45415728b3bbd6d4b3e9002cd
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2020-06-02 21:44:15 +0100

    remove json attributes

--- Recipeasy.Api/Models/Ingredient.cs
+++ Recipeasy.Api/Models/Ingredient.cs
@@ -1,16 +1,11 @@
-﻿using Newtonsoft.Json;
-
-namespace Recipeasy.Api.Models
+﻿namespace Recipeasy.Api.Models
 {
     public class Ingredient
     {
-        [JsonProperty("ingredientId")]
         public string IngredientId { get; set; }
 
-        [JsonProperty("ingredientName")]
         public string IngredientName { get; set; }
 
-        [JsonProperty("quantity")]
         public int Quantity { get; set; }
     }
 }
--- Recipeasy.Api/Models/Recipe.cs
+++ Recipeasy.Api/Models/Recipe.cs
@@ -1,20 +1,15 @@
-﻿using Newtonsoft.Json;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Recipeasy.Api.Models
 {
     public class Recipe
     {
-        [JsonProperty("recipeId")]
         public string RecipeId { get; set; }
 
-        [JsonProperty("recipeName")]
         public string RecipeName { get; set; }
 
-        [JsonProperty("ingredients")]
         public List<Ingredient> Ingredients { get; set; }
 
-        [JsonProperty("notes")]
         public string Notes { get; set; }
     }
 }
